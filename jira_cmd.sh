0ad100f870d3d4853ea110b8bbaec682ff42415f
Stephen McQuaid <steve@stevemcquaid.com>
2018-10-17 15:29:30 -0400

    Clean up jira_cmd.sh

--- jira_cmd.sh
+++ jira_cmd.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -ex
 
-# QUERY="project = K8S AND text ~ \"$@\""
-
-# /go/bin/jira --endpoint=$JIRA_ENDPOINT --user=$JIRA_EMAIL list -q "$QUERY"
-
 /go/bin/jira --endpoint=$JIRA_ENDPOINT --user=$JIRA_EMAIL "$@"
